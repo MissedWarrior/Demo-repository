93f870e47f2a587773523616c5ebd5127b5641bb
Rasul <gad-ras1@yandex.ru>
2019-11-21 12:12:53 +0300

    Configure empty store

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import './index.scss';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
+  <Provider store={createStore(() => {})}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('app'));
